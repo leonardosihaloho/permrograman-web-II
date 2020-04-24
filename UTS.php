37690bb41de79a2bde16280cb36b934c895418d8
leonardosihaloho <leonardosihaloho21@gmail.com>
2020-04-24 14:59:19 +0700

    UTS

---
+++ UTS.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+	<title>Leonardo Sihaloho 201614184</title>
+</head>
+<body>
+<P align="center">Data Pemantaun Covid19 wilayah DKI Jakarta"</p>
+<P align="center">Per 18 April 2020 21:38:16 (24 April 2020, 14.54 WIB.)</p>
+<P align="center"> LEONARDO SIHALOHO/2016141384</p>
+<form method="POST">
+			<table border="0">
+			<tr>
+			<td width="10%"> Jumlah Positif</td>
+			<td width="10%"> Jumlah Dirawat</td>
+			<td width="10%"> Jumlah Sembuh</td>
+			<td width="10%"> Jumlah Meninggal</td>
+			</tr>
+			<td><input type="text" name="jumlah_Positif">
+			</td>
+			<td><input type="text" name="jumlah_Positif">
+			</td>
+			<td><input type="text" name="jumlah_Positif">
+			</td>
+			<td><input type="text" name="Jumlah Meninggal">
+			<td>
+			</td>
+			<td><select name="Nama_Wilayah">
+			<option>Nama Wilayah</option>
+					<option value="DKI_Jakarta">DKI Jakarta</option>
+					<option value="Jawa_Barat">Jawa Barat</option>
+					<option value="Banten">Banten</option>
+					<option value="Jawa_Tengah">Jawa Tengah</option>
+			</td>
+			</table>
+<input type="submit" name="submit" value="proses">
+					<table border="1">			
+		<tr>
+		<td width="10%"> Jumlah Positif</td>
+		<td width="10%"> Jumlah Dirawat</td>
+		<td width="10%"> Jumlah Sembuh</td>
+		<td width="10%"> Jumlah Meninggal</td>
+		
+		</tr>
+		
+		<tr>
+		<td>2902</td>
+		<td>1769</td>
+		<td>206</td>
+		<td>257</td>
+		
+		</tr>
+	</table>
+	</form>
+</body>
+</html>
